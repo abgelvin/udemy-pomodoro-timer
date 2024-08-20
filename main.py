d0640fe9b5851faa8d13a442f3eb062da3edc94c
abgelvin <60162973+abgelvin@users.noreply.github.com>
2024-08-20 14:28:48 -0600

    timer and labels working

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -10,22 +10,68 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    global reps
+    reps += 1
+    work_sec = WORK_MIN
+    short_break_sec = SHORT_BREAK_MIN 
+    long_break_sec = LONG_BREAK_MIN
+
+
+    if reps % 8 == 0:
+        title.config(text='Break', fg=RED)
+        count_down(long_break_sec)
+    elif reps % 2 == 0 and reps < 8:
+        title.config(text='Break', fg=PINK)
+        count_down(short_break_sec)
+    else:
+        title.config(text='Work', fg=GREEN)
+        count_down(work_sec)
+   
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    minutes = math.floor(count / 60)
+    seconds = count % 60
+    canvas.itemconfig(timer_text, text=f'{minutes}:{seconds:02d}')
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+    
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title('Pomorodo')
+window.config(padx=100, pady=50, bg=YELLOW)
 
 
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+photo = PhotoImage(file='tomato.png')
+canvas.create_image(100, 112, image=photo)
+timer_text = canvas.create_text(100, 130, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
+canvas.grid(column=1, row=1)
 
+# count_down(5)
 
+title = Label(text='Timer', font=(FONT_NAME, 50, 'bold'), bg=YELLOW, fg=GREEN)
+title.grid(column=1, row=0)
 
+start = Button(text='Start', command=start_timer, bg=YELLOW, highlightthickness=0)
+start.grid(column=0, row=2)
+
+reset = Button(text='Reset', bg=YELLOW, highlightthickness=0)
+reset.grid(column=2, row=2)
+
+check_mark = Label(text='✓', font=(FONT_NAME, 25, 'bold'), bg=YELLOW, fg=GREEN)
+check_mark.grid(column=1, row=3)
 
 
 
